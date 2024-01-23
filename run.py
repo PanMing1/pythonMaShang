08ace7ca04008ab781cef5ce2baf10dccd34e9da
PanMing1 <1369431412@qq.com>
2024-01-23 18:03:48 +0800

    3

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
+import os
+import time
+
 import pytest
 
 from common.yaml_util import read_test_yaml
 
 if __name__ == '__main__':
-    pytest.main(['-vs'])
-    # pytest.main()
-    # write_yaml({"wheelsetCode": "109 M3C 05887"})
-    # print(read_test_yaml("/testcase/get_token.yaml"))
-    # clear_yaml()
+    pytest.main()
+    time.sleep(3)
+    os.system("allure generate ./temps -o ./reports --clean")
